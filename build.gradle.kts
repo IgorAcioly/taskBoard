2297ab3c6faf9ddc9b5fea88789df382253a4c3b
Igor <igor.g.acioly@gmail.com>
2025-08-05 22:21:05 -0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+group = "dio.java"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.liquibase:liquibase-core:4.29.1")
+    implementation("mysql:mysql-connector-java:8.0.33")
+    implementation("org.projectlombok:lombok:1.18.34")
+
+    annotationProcessor("org.projectlomboj:lombok:1.18.34")
+
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
